a96c3cd5f83d49d51e8fa07516451d624c6accef
Joe Chen <geometryolife@gmail.com>
2021-11-20 16:50:24 +0800

    add practice items for Chapter07

---
+++ Chapter07/practice/test-code-emacs/for-code.js
@@ -0,0 +1,18 @@
+function plus(var1) {
+    const alpha = 5;
+    console.log(plus(a)); // C-x C-l
+    const b;
+    const r = var1 + alpha;
+    return r;
+}
+
+// this is long long long long long long long long long long long comment
+function minus(var1) {
+    const delta = 5;
+    const rlt = var1 - delta;
+    return rlt;
+}
+
+const a = 10;
+console.log(plus(a));
+console.log(minus(a));
---
+++ Chapter07/practice/test-code-emacs/for-emacs.js
@@ -0,0 +1,15 @@
+function plus(var1) {
+  const delta = 5;
+  const r = var1 + delta;
+  return r;
+}
+
+function minus(var1) {
+  const five = 5;
+  const rlt =  var1 - five;
+  return rlt;
+}
+
+const a = 10;
+console.log(plus(a));
+console.log(minus(a));
